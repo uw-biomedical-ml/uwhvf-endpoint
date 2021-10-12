5ee667b0f140aee68aeaacd0212021b9e672e1c4
Aaron Lee <aaronylee@gmail.com>
2021-10-12 15:56:56 -0700

    update

---
+++ 1-filter.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import json
+from tqdm import tqdm
+import numpy as np
+
+
+def calcMD(hvf, td):
+    md = np.mean(td)
+    return md
+
+with open("alldata.json") as fin:
+    alldata = json.load(fin)
+alldata = alldata["data"]
+
+found = [0,0]
+with open("ptlvl.tsv", "w") as fout:
+    fout.write("ptid\teye\tgender\tyear\tstartmd\tstartmdcat\tage\tevent\ttime\n")
+    for ptid in tqdm(alldata.keys()):
+        ld = None
+        seq = {}
+        gender = alldata[ptid]["gender"]
+        year = alldata[ptid]["year"]
+        for eye in ("R", "L"):
+            if not eye in alldata[ptid]:
+                continue
+            firstage = None
+            age = None
+            last = None
+            for dat in alldata[ptid][eye]:
+                age = dat["age"]
+                if firstage == None:
+                    firstage = age
+                    startmd = np.mean(dat["td_seq"])
+                    seq[0.0] = dat["td_seq"]
+                    last = age
+                if age - last > 2.25:
+                    break
+                seq[age - firstage] = np.array(dat["td_seq"])
+                last = age
+            event = 0
+            ld = seq[0.0]
+            last = np.zeros(ld.shape, dtype=np.uint8)
+            for d in sorted(seq.keys()):
+                if d == 0.0:
+                    continue
+                diff = seq[d] - ld
+                #print(diff)
+                curdiff = (diff <= -7.0 ).astype(np.uint8)
+                accum = last + curdiff
+                #print(accum)
+                db7 = np.sum(accum == 2)
+                last = curdiff
+                #print(d, db7)
+                if db7 >= 5:
+                    event = 1
+                    time = d
+                    break
+                time = d
+            startmdcat = None
+            if startmd > -5:
+                startmdcat = 0
+            elif startmd <= -5 and startmd > -10:
+                startmdcat = 1
+            elif startmd <= -10 and startmd > -15:
+                startmdcat = 2
+            elif startmd <= -15:
+                startmdcat = 3
+            if time == 0:
+                continue
+            if time >= 5.0:
+                found[event] += 1
+            out = (ptid, eye, gender, year, startmd, startmdcat, firstage, event, time)
+            fout.write("%s\n" % "\t".join(map(str,out)))
+print(found)
---
+++ 2-graph.R
@@ -0,0 +1,132 @@
+#!/usr/bin/env Rscript
+
+library(survminer)
+library(survival)
+
+dat <- read.csv("ptlvl.tsv", sep="\t")
+dat <- dat[which(dat$age > 18),]
+
+dat$eye <- factor(dat$eye)
+dat$gender <- factor(dat$gender)
+
+dat$agecat <- NULL
+dat$agecat[dat$age < 50] <- "< 50"
+dat$agecat[dat$age >= 50 & dat$age < 60] <- "50 - 60"
+dat$agecat[dat$age >= 60 & dat$age < 70] <- "60 - 70"
+dat$agecat[dat$age >= 70 & dat$age < 80] <- "70 - 80"
+dat$agecat[dat$age >= 80 ] <- "> 80"
+
+dat$agecat <- factor(dat$agecat, levels=c("< 50", "50 - 60", "60 - 70", "70 - 80", "> 80"))
+
+
+dat$startmdcat <- NULL
+dat$startmdcat[dat$startmd > -2.5] <- "> -2.5"
+dat$startmdcat[dat$startmd > -5.0 & dat$startmd <= -2.5] <- "-5 to -2.5"
+dat$startmdcat[dat$startmd > -10.0 & dat$startmd <= -5] <- "-10 to -5"
+dat$startmdcat[dat$startmd > -15.0 & dat$startmd <= -10] <- "-15 to -10"
+dat$startmdcat[dat$startmd > -20.0 & dat$startmd <= -15] <- "-20 to -15"
+dat$startmdcat[dat$startmd < -20.0] <- "< -20"
+
+dat$startmdcat <- factor(dat$startmdcat, levels=c("> -2.5", "-5 to -2.5", "-10 to -5", "-15 to -10", "-20 to -15", "< -20"))
+
+dat$yearcat <- NULL
+dat$yearcat[dat$year < 2005] <- "< 2005"
+dat$yearcat[dat$year >= 2005 & dat$year < 2010] <- "2005 - 2010"
+dat$yearcat[dat$year >= 2010 & dat$year < 2015] <- "2010 - 2015"
+dat$yearcat[dat$year >= 2015] <- "> 2015"
+
+dat$yearcat <- factor(dat$yearcat, levels=c("< 2005", "2005 - 2010", "2010 - 2015", "> 2015"))
+
+summary(dat)
+
+pdf("full.pdf")
+fit <- survfit(Surv(time, event) ~ 1, data = dat)
+ggsurvplot(
+  fit, 
+  data = dat, 
+  size = 1,                 # change line size
+  conf.int = TRUE,          # Add confidence interval
+  pval = TRUE,              # Add p-value
+  palette = "jco",
+  risk.table = TRUE,        # Add risk table
+  risk.table.col = "strata",# Risk table color by groups
+  risk.table.height = 0.25, # Useful to change when you have multiple groups
+  xlim = c(0, 15),
+  ggtheme = theme_bw()      # Change ggplot2 theme
+)
+dev.off()
+
+
+
+pdf("agecat.pdf")
+fit <- survfit(Surv(time, event) ~ agecat, data = dat)
+ggsurvplot(
+  fit, 
+  data = dat, 
+  size = 1,                 # change line size
+  conf.int = TRUE,          # Add confidence interval
+  pval = TRUE,              # Add p-value
+  palette = "jco",
+  risk.table = TRUE,        # Add risk table
+  risk.table.col = "strata",# Risk table color by groups
+  risk.table.height = 0.25, # Useful to change when you have multiple groups
+  xlim = c(0, 15),
+  ggtheme = theme_bw()      # Change ggplot2 theme
+)
+dev.off()
+
+
+pdf("mdcat.pdf")
+fit <- survfit(Surv(time, event) ~ startmdcat, data = dat)
+ggsurvplot(
+  fit, 
+  data = dat, 
+  size = 1,                 # change line size
+  conf.int = TRUE,          # Add confidence interval
+  pval = TRUE,              # Add p-value
+  palette = "jco",
+  risk.table = TRUE,        # Add risk table
+  risk.table.col = "strata",# Risk table color by groups
+  risk.table.height = 0.25, # Useful to change when you have multiple groups
+  xlim = c(0, 15),
+  ggtheme = theme_bw()      # Change ggplot2 theme
+)
+dev.off()
+
+
+
+pdf("facet.pdf")
+fit <- survfit(Surv(time, event) ~ startmdcat, data = dat)
+ggsurvplot_facet(
+  fit, 
+  data = dat, 
+  facet.by = "agecat",
+  size = 1,                 # change line size
+  conf.int = TRUE,          # Add confidence interval
+  pval = TRUE,              # Add p-value
+  palette = "jco",
+  xlim = c(0, 15),
+  ggtheme = theme_bw()      # Change ggplot2 theme
+)
+dev.off()
+
+
+pdf("facet-year.pdf")
+fit <- survfit(Surv(time, event) ~ yearcat, data = dat)
+ggsurvplot_facet(
+  fit, 
+  data = dat, 
+  facet.by = "startmdcat",
+  size = 1,                 # change line size
+  conf.int = TRUE,          # Add confidence interval
+  pval = TRUE,              # Add p-value
+  palette = "jco",
+  xlim = c(0, 15),
+  ggtheme = theme_bw()      # Change ggplot2 theme
+)
+dev.off()
+
+
+
+
+
