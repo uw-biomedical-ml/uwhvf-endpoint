bd6c016b8c28c9410644c964a36f38b375df1276
Aaron Lee <aaronylee@gmail.com>
2021-10-17 10:41:08 -0700

    proper power

--- samplesize/2-graph.R
+++ samplesize/2-graph.R
@@ -6,7 +6,7 @@ pow80 <- read.csv("simulation.csv")
 
 pow80$effect.size <- 100.0 * pow80$effect.size
 pow80$Years <- factor(pow80$length)
-p <- ggplot(pow80, aes(effect.size, sample.size, color=Years, group=Years)) + geom_point() + geom_smooth() + scale_color_brewer(palette="Set1") + scale_y_log10() + xlab("Effect size as percent reduction in event rate") + ylab("Number of patients needed in each arm") + facet_grid(~setting)
+p <- ggplot(pow80, aes(effect.size, sample.size, color=setting, group=setting)) + geom_point() + geom_smooth() + scale_color_brewer(palette="Set1") + scale_y_log10() + xlab("Effect size as percent reduction in event rate") + ylab("Number of patients needed in each arm") + facet_grid(Years~.)
 ggsave("pow80.png", p)
 
 quit()
