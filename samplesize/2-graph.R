0e8c1f8cc137c969ce11ec5ecaef037332c82154
Aaron Lee <aaronylee@gmail.com>
2021-10-18 08:11:17 -0700

    update

--- samplesize/2-graph.R
+++ samplesize/2-graph.R
@@ -7,7 +7,7 @@ pow80 <- read.csv("simulation.csv")
 pow80$effect.size <- 100.0 * pow80$effect.size
 pow80 <- pow80[which(pow80$effect.size <= 30),]
 pow80$Years <- factor(pow80$length)
-p <- ggplot(pow80, aes(effect.size, sample.size, color=setting, group=setting)) + geom_point() + geom_line() + scale_color_brewer(palette="Set1") + xlab("Effect size as percent reduction in event rate") + ylab("Number of patients needed in each arm") + facet_grid(Years~., scales="free_y")
+p <- ggplot(pow80, aes(effect.size, sample.size, color=setting, group=setting)) + geom_point() + geom_line() + scale_color_brewer(palette="Set1") + xlab("Effect size as percent reduction in event rate") + ylab("Number of patients needed in each arm") + facet_grid(Years~., scales="free_y") + scale_y_log10()
 ggsave("pow80.png", p)
 
 quit()
