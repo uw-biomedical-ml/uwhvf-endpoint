2b27a1bf85f07f70c906753883ac2806557e9bfd
Aaron Lee <aaronylee@gmail.com>
2021-10-18 09:24:57 -0700

    update

--- samplesize/2-graph.R
+++ samplesize/2-graph.R
@@ -6,8 +6,15 @@ pow80 <- read.csv("simulation.csv")
 
 pow80$setting <- factor(pow80$setting, levels=c("base", "subgroup", "model"), labels=c("No selection", "Traditional Subgroup", "AI Model"))
 pow80$effect.size <- 100.0 * pow80$effect.size
-pow80 <- pow80[which(pow80$effect.size <= 30),]
 pow80$Years <- factor(pow80$length)
+
+effect20 <- pow80[which(pow80$effect.size == 20 | pow80$effect.size == 10 | pow80$effect.size == 30 | pow80$effect.size == 50),]
+p <- ggplot(effect20, aes(Years, sample.size, color=setting, group=setting)) + geom_point() + geom_line() +
+    scale_color_brewer(palette="Set1") + ylim(0,10000) + facet_wrap(~effect.size ) + 
+    ylab("Number of patients needed in each arm") + theme(legend.title = element_blank())
+ggsave("effect20.png", p, width=7, height=7)
+
+pow80 <- pow80[which(pow80$effect.size <= 30),]
 p <- ggplot(pow80, aes(effect.size, sample.size, color=setting, group=setting)) + geom_point() + geom_line() + 
     scale_color_brewer(palette="Set1") + xlab("Effect size as percent reduction in event rate") + 
     ylab("Number of patients needed in each arm") + 
@@ -15,12 +22,6 @@ p <- ggplot(pow80, aes(effect.size, sample.size, color=setting, group=setting)) 
     theme(legend.title = element_blank()) +
     facet_wrap(Years~., scales="free_y") 
 ggsave("pow80.png", p, width=12, height=6)
-
-
-effect20 <- pow80[which(pow80$effect.size == 20 | pow80$effect.size == 10 | pow80$effect.size == 30 | pow80$effect.size == 50),]
-p <- ggplot(effect20, aes(Years, sample.size, color=setting, group=setting)) + geom_point() + geom_line() +
-    scale_color_brewer(palette="Set1") + ylim(0,10000) + facet_wrap(effect.size ~. )
-ggsave("effect20.png", p, width=7, height=7)
 
 quit()
 
