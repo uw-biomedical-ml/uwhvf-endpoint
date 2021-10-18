19880194b5574bd5491c5cb0edd08c391a6828a2
Aaron Lee <aaronylee@gmail.com>
2021-10-18 14:42:20 -0700

    update

--- bootstrap-roc-prc/graph2.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-
-library(ggplot2)
-dat <- read.csv("model.csv")
-
-p <- ggplot(dat, aes(factor(status), X1, color=time)) + geom_point(position="jitter")
-ggsave("coxrr.png", p)
